0a683283908b2a71925bc37e47db86c00a527d56
Mack Ward <mackorone@gmail.com>
2014-08-19 23:15:09 -0400

    Small refactor

--- printPath.py
+++ printPath.py
@@ -1,14 +1,6 @@
 import os
 import sys
+from getPath import getPath
 
-# Prints the path to <target> if it's the current directory or
-# any ancestor directories of <directory>, otherwise prints nothing
-def getPath(directory, target):
-    while (directory != ''):
-        if target in [item for item in os.listdir(directory)]:
-            return directory + '/' + target
-        directory = directory[:directory.rfind('/')]
-
-# This should never be false, so long as the 'cd.sh' is correct
 if (1 < len(sys.argv)):
     print getPath(os.getcwd(), sys.argv[1])
